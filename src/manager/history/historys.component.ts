ebf3dc0cc65a8d8128723178bb56480e6e8e570e
Huang Yuan haung <dha834773759@163.com>
2019-12-25 22:32:20 +0800

    queryparm

--- src/manager/history/historys.component.ts
+++ src/manager/history/historys.component.ts
@@ -6,16 +6,17 @@ import { appModuleAnimation } from '@shared/animations/routerTransition';
 import { PagedListingComponentBase, PagedRequestDto } from '@shared/paged-listing-component-base';
 import { OperationLogDto, OperationLogServiceProxy, OperationLogDtoPagedResultDto } from '@shared/service-proxies/service-proxies';
 import { finalize } from 'rxjs/operators';
+import { MatDatepickerInputEvent } from '@angular/material';
 
-
+import * as moment from 'moment';
 
 
 class PagedOperationLogInfoRequestDto extends PagedRequestDto {
-  module: string;
-  date: string;
-  method: string;
-  page: string;
-  userId: number;
+  public module: string = '';
+  public date: string = '';
+  public method: string = '';
+  public page: string = '';
+  public userId: number = 0;
 }
 
 @Component({
@@ -28,13 +29,13 @@ class PagedOperationLogInfoRequestDto extends PagedRequestDto {
 export class HistorysComponent extends PagedListingComponentBase<OperationLogDto> {
   historyNodes: OperationLogDto[] = [];
 
-  keyword = '';
-
+  query: PagedOperationLogInfoRequestDto;
   constructor(
     injector: Injector,
     private _proxy: OperationLogServiceProxy
   ) {
     super(injector);
+    this.query = new PagedOperationLogInfoRequestDto();
   }
 
   list(
@@ -42,11 +43,9 @@ export class HistorysComponent extends PagedListingComponentBase<OperationLogDto
     pageNumber: number,
     finishedCallback: Function
   ): void {
-
-    request.module = this.keyword;
-
+    console.log(this.query);
     this._proxy
-      .getAll(request.module, request.page, request.method, request.date, request.userId, request.skipCount, request.maxResultCount)
+      .getAll(this.query.module, this.query.page, this.query.method, this.query.date, this.query.userId, request.skipCount, request.maxResultCount)
       .pipe(
         finalize(() => {
           finishedCallback();
@@ -57,7 +56,15 @@ export class HistorysComponent extends PagedListingComponentBase<OperationLogDto
         this.showPaging(result, pageNumber);
       });
   }
+  dateChange(data: MatDatepickerInputEvent<Date>) {
+    if (data.value) {
+      this.query.date = moment(data.value).format('YYYY-MM-DD');
 
+    } else {
+      this.query.date = '';
+    }
+
+  }
   delete(role: OperationLogDto): void {
 
   }
