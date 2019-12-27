dedbf2785433d633b615e1a3b6ac33cddc5a79ab
jinji.wu <aoe123jiji@163.com>
2019-12-27 15:33:12 +0800

    工艺库

--- src/manager/library/view-library/edge-library/edge-library.component.ts
+++ src/manager/library/view-library/edge-library/edge-library.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
+import { MatTableDataSource, MatSort } from '@angular/material';
 
 @Component({
   selector: 'app-edge-library',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edge-library.component.css']
 })
 export class EdgeLibraryComponent implements OnInit {
+  displayedColumns: string[] = ['machiningKindName', 'materialThickness', 
+  'materialName', 'gasName', 
+  'nozzleKindName',  'eNo', 
+  'nozzleDiameter','angle',
+  'power', 'frequency', 
+  'duty', 'gasPressure', 
+  'piercingTime', 'recoveryDistance', 'recoveryFrequency',
+  'recoveryFeedrate', 'recoveryDuty', 
+  'gap','gapAxis', 
+  'beamSpot', 'focalPosition', 
+  'liftDistance', 'pbPower'];
+  dataSource = new MatTableDataSource([]);
+
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
   constructor() { }
 
   ngOnInit() {
-   
   }
 
+  show(data: any[]) {
+    this.dataSource = new MatTableDataSource(data);
+    this.dataSource.sort = this.sort;
+  }
 }
