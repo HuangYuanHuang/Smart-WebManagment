23efaae61237549b1cb2263bf869f553963ae1e1
Huang Yuan haung <dha834773759@163.com>
2019-12-30 15:27:48 +0800

    sidebar

--- src/layout/sidebar/sidebar-nav.component.ts
+++ src/layout/sidebar/sidebar-nav.component.ts
@@ -25,22 +25,22 @@ export class SideBarNavComponent extends AppComponentBase {
 
         new MenuItem(this.l('Monitor'), 'Web.Manager.Monitors', 'info', '/le/monitor'),
 
-        new MenuItem(this.l('MultiLevelMenu'), '', 'menu', '', [
-            new MenuItem('ASP.NET Boilerplate', '', '', '', [
-                new MenuItem('Home', '', '', 'https://aspnetboilerplate.com/?ref=abptmpl'),
-                new MenuItem('Templates', '', '', 'https://aspnetboilerplate.com/Templates?ref=abptmpl'),
-                new MenuItem('Samples', '', '', 'https://aspnetboilerplate.com/Samples?ref=abptmpl'),
-                new MenuItem('Documents', '', '', 'https://aspnetboilerplate.com/Pages/Documents?ref=abptmpl')
-            ]),
-            new MenuItem('ASP.NET Zero', '', '', '', [
-                new MenuItem('Home', '', '', 'https://aspnetzero.com?ref=abptmpl'),
-                new MenuItem('Description', '', '', 'https://aspnetzero.com/?ref=abptmpl#description'),
-                new MenuItem('Features', '', '', 'https://aspnetzero.com/?ref=abptmpl#features'),
-                new MenuItem('Pricing', '', '', 'https://aspnetzero.com/?ref=abptmpl#pricing'),
-                new MenuItem('Faq', '', '', 'https://aspnetzero.com/Faq?ref=abptmpl'),
-                new MenuItem('Documents', '', '', 'https://aspnetzero.com/Documents?ref=abptmpl')
-            ])
-        ])
+        // new MenuItem(this.l('MultiLevelMenu'), '', 'menu', '', [
+        //     new MenuItem('ASP.NET Boilerplate', '', '', '', [
+        //         new MenuItem('Home', '', '', 'https://aspnetboilerplate.com/?ref=abptmpl'),
+        //         new MenuItem('Templates', '', '', 'https://aspnetboilerplate.com/Templates?ref=abptmpl'),
+        //         new MenuItem('Samples', '', '', 'https://aspnetboilerplate.com/Samples?ref=abptmpl'),
+        //         new MenuItem('Documents', '', '', 'https://aspnetboilerplate.com/Pages/Documents?ref=abptmpl')
+        //     ]),
+        //     new MenuItem('ASP.NET Zero', '', '', '', [
+        //         new MenuItem('Home', '', '', 'https://aspnetzero.com?ref=abptmpl'),
+        //         new MenuItem('Description', '', '', 'https://aspnetzero.com/?ref=abptmpl#description'),
+        //         new MenuItem('Features', '', '', 'https://aspnetzero.com/?ref=abptmpl#features'),
+        //         new MenuItem('Pricing', '', '', 'https://aspnetzero.com/?ref=abptmpl#pricing'),
+        //         new MenuItem('Faq', '', '', 'https://aspnetzero.com/Faq?ref=abptmpl'),
+        //         new MenuItem('Documents', '', '', 'https://aspnetzero.com/Documents?ref=abptmpl')
+        //     ])
+        // ])
     ];
 
     constructor(
